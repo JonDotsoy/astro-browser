096834d40ea75228db9ecbb7716ef0d388c23143
Jonathan Delgado <hi@jon.soy>
2023-07-07 13:29:26 -0400

    feat: declare mod file

--- src/Options.ts
+++ src/Options.ts
@@ -1,5 +1,4 @@
 export interface Options {
-  puppeteer: any;
   /** Main page to connect */
   url: URL;
   headless?: boolean;
---
+++ src/mod.ts
@@ -0,0 +1,2 @@
+export { Astro } from "./Astro.ts";
+export type { Options } from "./Options.ts";
